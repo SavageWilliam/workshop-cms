275fc99ada5232e3a4e98b33bb7000173dc81a7c
WilliamSavage <williamsavage@hotmail.com>
2016-11-07 15:55:04 +0000

    css and image files dealt with

--- server.js
+++ server.js
@@ -1,26 +1,30 @@
 var http = require('http');
 var fs = require('fs');
 var port = 3000; // Not saved in a variable in Node Girls tutorial. But isn't this pretty?!!!
+var path = require('path');
 
 var message = "Welcome to the best server in the world!";
 
 function handler(request, response) {
   var endpoint = request.url;
-  console.log(endpoint);
-  var method = request.method;
-  console.log(method);
-  if (endpoint === '/') {
-    response.writeHead(200, {"Content-type": "text/html"});
+  console.log(endpoint, "ENDPOINT");
 
-    fs.readFile(__dirname + '/public/index.html', function(error, file) {
+  var extension = endpoint.split(".")[1];
+  console.log(extension, "EXTENSION");
+
+  var method = request.method;
+
+
+  if (endpoint === '/') {
+    fs.readFile(path.join(__dirname, 'public/index.html'), function(error, file) {
       if (error) {
         console.log(error);
         return;
       }
+      response.writeHead(200, {"Content-type": "text/html"});
       response.end(file);
     })
-  }
-  else if (endpoint === '/node') {
+  } else if (endpoint === '/node') {
     response.writeHead(200, {"Content-type": "text/html"});
     response.write('Oh my goodness, you\'re in node');
     response.end();
@@ -28,6 +32,16 @@ function handler(request, response) {
     response.writeHead(200, {"Content-type": "text/html"});
     response.write('Run the world\, girls');
     response.end();
+  } else if (endpoint.indexOf('css') !== -1 || endpoint.indexOf('jpg') !== -1) {
+    fs.readFile(path.join(__dirname, 'public', endpoint), function(err, file) {         
+      console.log(__dirname, "DIRECTORY NAME");
+      if(err) {
+        console.log(error);
+        return;
+      }
+      response.writeHead(200, {"Content-type": "text/" + extension});
+      response.end(file);
+    })
   } else {
     response.writeHead(200, {"Content-type": "text/html"});
     response.write(message);  //response body
